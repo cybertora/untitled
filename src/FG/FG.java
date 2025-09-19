43457c615190f7d7be4705cd38cf4d70f96022c8
rassul <rtokatov@gmail.com>
2025-09-19 18:31:58 +0500

    ро

--- src/FG/FG.java
+++ src/FG/FG.java
@@ -7,12 +7,13 @@ public class FG {
     private String chain;
     private String saddle;
 
-    private FG(Builder builder) {
-        this.bars = builder.bars;
-        this.frame = builder.frame;
-        this.system = builder.system;
-        this.chain = builder.chain;
-        this.saddle = builder.saddle;
+
+    private FG(Builder b) {
+        this.bars = b.bars;
+        this.frame = b.frame;
+        this.system = b.system;
+        this.chain = b.chain;
+        this.saddle = b.saddle;
     }
 
     public String toString() {
@@ -20,8 +21,8 @@ public class FG {
                 "bars= " + bars +
                 ", frameMaterial= '" + frame + '\'' +
                 ", system= '" + system + '\'' +
-                ", chain= ' " + chain + '\'' +
-                ", saddle=' " + saddle;
+                ", chain= '" + chain + '\'' +
+                ", saddle='" + saddle + '\'';
     }
 
     public static class Builder{
@@ -29,10 +30,7 @@ public class FG {
         private String frame = "steel";
         private String system = "beclass";
         private String chain = "S1";
-        private String saddle = "Givny";
-
-        public Builder() {
-        }
+        private String saddle = "givny";
 
         public Builder bars(String bars){
             this.bars = bars;
@@ -54,7 +52,7 @@ public class FG {
             this.saddle = saddle;
             return this;
         }
-        
+
 
         public FG build(){
             return new FG(this);
